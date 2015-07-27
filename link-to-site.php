6a4c737d85446246dcc2e0fa678ebdecaea7bac9
Fabian Altahona <fabolivar@gmail.com>
2015-07-27 11:11:43 -0500

    Add plugin URI

--- link-to-site.php
+++ link-to-site.php
@@ -1,7 +1,7 @@
 <?php
 /*
   * Plugin Name:       Link To Site
-  * Plugin URI:        https://koombea.com/
+  * Plugin URI:        http://koombea.github.io/link-to-site/
   * Description:       The Link To Site plugin allows you to create a link style post commonly seen in blogs like DaringFireball.net, Marco.org, MacStories.net and SixColors.com. This means that you can link from your blog home or from RSS directly to an external page and not to your post's permalink.
   * Author:            Koombea
   * Contributors:      koombea
